9f6d447ea012380a38fca99577c6b11fc9864683
Sora G.D. <25464989+Sora-Andrea@users.noreply.github.com>
2025-10-07 23:20:10 -0400

    Preparing camera feature (installed packages, created initial layout page)

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -24,6 +24,14 @@ export default function TabLayout() {
         }}
       />
 
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: "Camera", 
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
+        }}
+      />
+
       <Tabs.Screen 
         name="risk" 
         options={{
